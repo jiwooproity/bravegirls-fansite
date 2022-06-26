a2ac4d69866ec26ee8cbabd92ad9cd7175311764
Sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2022-06-26 14:54:00 +0900

    Brave_003/다운로드 기능 및 슬라이드 추가

--- src/components/page/main/CardSection.jsx
+++ src/components/page/main/CardSection.jsx
@@ -3,11 +3,15 @@ import _ from "lodash";
 
 import styled from "styled-components";
 
-import { photocardService } from "../../../service/photocardService";
+import Slider from "react-slick";
+import { onDownload, photocardService } from "../../../service/photocardService";
+
+import "../../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../../node_modules/slick-carousel/slick/slick-theme.css";
 
 const CardSectionContainer = styled.div`
   width: 100%;
-  height: 500px;
+  height: 550px;
   padding: 10px 0px;
   display: flex;
   flex-direction: column;
@@ -52,36 +56,36 @@ const CardSectionGubun = styled.button`
 
 const CardSectionWrapper = styled.div`
   width: 990px;
-  margin: 15px 0px;
-  display: flex;
-  justify-content: space-between;
+  padding: 0px 0px;
 `;
 
 const CardWrapper = styled.div`
   width: 230px;
-  padding: 0px 0px;
+  padding: 15px 0px;
+  position: relative;
 `;
 
 const CardImage = styled.img`
-  width: 100%;
+  width: 230px;
   display: block;
   border-radius: 8px;
-  background-color: black;
-  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
 
-  &:hover {
+  position: relative;
+
+  box-shadow: rgba(0, 0, 0, 0.12) 2px 3px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+
+  /* &:hover {
     transform: translateY(-10px);
   }
 
   transition: transform 0.5s ease;
-  cursor: pointer;
+  cursor: pointer; */
 `;
 
 const CardDescriptionArea = styled.div`
   width: 100%;
+  height: 100%;
   padding: 5px 0px;
-  display: flex;
-  flex-direction: column;
 `;
 
 const CardDescriptionTitle = styled.h1`
@@ -101,11 +105,40 @@ const LoadingText = styled.h1`
   font-size: 20px;
 `;
 
+const CustomSlider = styled(Slider)`
+  .slick-slide div {
+    outline: none; // 슬라이드 클릭시 파란선을 제거하기 위해서 작성
+
+    padding-right: 10px;
+  }
+
+  .slick-prev {
+    z-index: 5;
+  }
+
+  .slick-next {
+    z-index: 5;
+  }
+`;
+
 const CardSection = () => {
   const [photoCard, setPhotoCard] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const gubunArr = ["RED SUN", "MVSK"];
+  const settings = {
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    centerPadding: "-10px",
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  const gubunArr = ["# RED SUN", "# MVSK"];
 
   useEffect(() => {
     loadData();
@@ -138,14 +171,18 @@ const CardSection = () => {
 
       <CardSectionWrapper>
         {loading ? (
-          _.map(photoCard, (card, index) => (
-            <CardWrapper key={index}>
-              <CardImage src={card.photocard_image} />
-              <CardDescriptionArea>
-                <CardDescriptionTitle>{card.photocard_name}</CardDescriptionTitle>
-              </CardDescriptionArea>
-            </CardWrapper>
-          ))
+          <CustomSlider {...settings}>
+            {_.map(photoCard, (card, index) => (
+              <CardWrapper key={Number(index)}>
+                <a href={card.photocard_image} download onClick={(e) => onDownload(e, card.member_name, card.photocard_name)}>
+                  <CardImage src={card.photocard_image} download={card.photocard_name} />
+                </a>
+                <CardDescriptionArea>
+                  <CardDescriptionTitle>{card.photocard_name}</CardDescriptionTitle>
+                </CardDescriptionArea>
+              </CardWrapper>
+            ))}
+          </CustomSlider>
         ) : (
           <LoadingWrapper>
             <LoadingText>Loading...</LoadingText>
--- src/service/photocardService.js
+++ src/service/photocardService.js
@@ -25,3 +25,33 @@ const settingMethod = async (url, params, method) => {
 export const photocardService = {
   getPhotocardData: (params) => settingMethod("photocard/getPhotocard.php", params, "GET"),
 };
+
+export const onDownload = async (e, member_name, photocardName) => {
+  e.preventDefault();
+
+  if (window.confirm(`[${member_name}] ${photocardName} 시안을 다운로드 받으시겠습니까?`)) {
+    const image = e.target.src;
+
+    // 다운로드 파일 이름 지정
+    const image_split = image.split(".");
+    const image_url = image_split[image_split.length - 1];
+
+    // URL 이미지 다운로드 구현
+    fetch(image, {
+      method: "GET",
+      header: {},
+    }).then((response) => {
+      response.arrayBuffer().then((buffer) => {
+        const url = window.URL.createObjectURL(new Blob([buffer]));
+        const link = document.createElement("a");
+
+        link.href = url;
+        link.setAttribute("download", `${member_name}_${photocardName}.${image_url}`);
+
+        document.body.appendChild(link);
+
+        link.click();
+      });
+    });
+  }
+};
