ddd6ad083754a94a7bf7fc51d30a36c71d57b9fa
sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2022-07-28 14:24:50 +0900

    Brave_025/앨범 페이지 : 사이드 바 추가

--- src/components/page/album/AlbumSidebar.jsx
+++ src/components/page/album/AlbumSidebar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
-const AlbumSidebar = () => {
-  return <div></div>;
+import _ from "lodash";
+
+const AlbumSidebarContainer = styled.div`
+  width: 400px;
+  height: 100vh;
+
+  position: fixed;
+  top: 0;
+  right: 0;
+
+  background-color: black;
+`;
+
+const AlbumSidebar = (props) => {
+  const { data } = props;
+  console.log(data);
+
+  return <AlbumSidebarContainer></AlbumSidebarContainer>;
 };
 
 export default AlbumSidebar;
