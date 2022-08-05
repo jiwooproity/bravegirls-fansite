b61927c734afdc7e2c8224a7c9acbe532b6be1c4
sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2022-08-05 17:19:59 +0900

    파일 정리 [4]

--- src/components/page/album/Album.jsx
+++ src/components/page/album/Album.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import styled from "styled-components";
 import _ from "lodash";
 
 import { AlbumInfo, AlbumList, AlbumTrack } from "components";
@@ -10,77 +9,12 @@ import { utils } from "util";
 import { useStore } from "hooks";
 import { musicService } from "service";
 
-const DarkThemeMode = styled.div`
-  width: 100%;
-  height: 500px;
-
-  opacity: ${({ active }) => (active === "true" ? "1" : "0")};
-
-  position: absolute;
-  top: 0;
-  left: 0;
-
-  overflow: hidden;
-
-  @media screen and (max-width: 768px) {
-    height: 500px;
-  }
-
-  transition: opacity 1s ease;
-`;
-
-const DarkThemeImage = styled.img`
-  width: 100%;
-  height: 100%;
-
-  display: block;
-
-  position: absolute;
-  top: 0;
-  left: 0;
-
-  object-fit: cover;
-
-  transform: scale(30);
-
-  z-index: 0;
-`;
-
-const DarkThemeBackdrop = styled.div`
-  width: 100%;
-  height: 100%;
-
-  position: absolute;
-  top: 0;
-  left: 0;
-
-  background: ${(props) =>
-    `linear-gradient(180deg, hsla(0, 0%, 100%, 0), ${props.theme.backgroundColor})`};
-
-  background-color: ${({ active }) =>
-    active === "true" ? "rgba(54, 54, 54, 0.5)" : ""};
-  backdrop-filter: blur(150px);
-
-  z-index: 1;
-`;
-
-const AlbumContainer = styled.div`
-  width: 100%;
-  min-height: calc(100vh - 85px);
-
-  padding: 0px 15px 30px 15px;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  position: relative;
-
-  @media screen and (max-width: 768px) {
-    padding: 0px 15px 30px 15px;
-  }
-`;
+import {
+  DarkThemeMode,
+  DarkThemeImage,
+  DarkThemeBackdrop,
+  AlbumContainer,
+} from "style";
 
 const Album = () => {
   const { themeStore } = useStore();
--- src/style/index.js
+++ src/style/index.js
@@ -1,4 +1,76 @@
 import GlobalStyle from "./GlobalStyle";
 import { theme } from "./Theme";
 
+import {
+  DarkThemeMode,
+  DarkThemeImage,
+  DarkThemeBackdrop,
+  AlbumContainer,
+} from "./AlbumPage/album";
+
+import {
+  AlbumWrapper,
+  AlbumLeftSide,
+  AlbumImageWrap,
+  AlbumImage,
+  LpImageWrap,
+  LpImage,
+  AlbumRightSide,
+  AlbumDescription,
+  AlbumTitleText,
+  DescriptionText,
+  AlbumEntertainment,
+} from "./AlbumPage/albumInfo";
+
+import {
+  AlbumImage as AlbumThumbnail,
+  AlbumListShadowWrapper,
+  AlbumListWrapper,
+  AlbumSelectBar,
+  AlbumListShadow,
+  AlbumColumnWrapper,
+  AlbumColumn,
+  AlbumLists,
+  AlbumListCover,
+  AlbumNumber,
+  AlbumInfoColum,
+  AlbumInfoDummy,
+  AlbumInfo,
+  AlbumListIcon,
+} from "./AlbumPage/albumList";
+
+import {
+  TrackArtist,
+  TrackContainer,
+  TrackItemsWrap,
+  TrackList,
+  TrackListBox,
+  TrackListTitle,
+  TrackListWrapper,
+  TrackNumber,
+  TrackSubTitle,
+  TrackTitle,
+  TrackTitleWrap,
+} from "./AlbumPage/albumTrack";
+
 export { GlobalStyle, theme };
+
+// 앨범 컴포넌트 CSS 스타일
+// Album.jsx
+export { DarkThemeMode, DarkThemeImage, DarkThemeBackdrop, AlbumContainer };
+
+// AlbumInfo.jsx
+export { AlbumWrapper, AlbumLeftSide, AlbumImageWrap, AlbumImage };
+export { LpImageWrap, LpImage, AlbumRightSide, AlbumDescription };
+export { AlbumTitleText, DescriptionText, AlbumEntertainment };
+
+// AlbumList.jsx
+export { AlbumThumbnail, AlbumListShadowWrapper, AlbumListWrapper };
+export { AlbumSelectBar, AlbumListShadow, AlbumColumnWrapper, AlbumColumn };
+export { AlbumLists, AlbumListCover, AlbumNumber, AlbumInfoColum };
+export { AlbumInfoDummy, AlbumInfo, AlbumListIcon };
+
+// TrackList.jsx
+export { TrackArtist, TrackContainer, TrackItemsWrap, TrackList };
+export { TrackListBox, TrackListTitle, TrackListWrapper, TrackNumber };
+export { TrackSubTitle, TrackTitle, TrackTitleWrap };
