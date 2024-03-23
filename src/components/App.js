24eaea7a8d572d3e0d49c201e7ececc23a2eeea3
jiwooproity <jiwooproity@naver.com>
2024-03-23 14:21:05 +0900

    Fix: 컴포넌트 코드 분할 및 Member 이미지 최적화

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useObserver } from "mobx-react";
 
@@ -6,11 +6,10 @@ import _ from "lodash";
 
 import styled, { ThemeProvider } from "styled-components";
 
-import { Main, CanvasBoard, CanvasDetail, Loading, AnimateTitle } from "components";
+import { CanvasBoard, CanvasDetail, Loading, AnimateTitle } from "components";
 
 import { Login, Success, Register, Toast } from "components";
 
-import { Member, Album, Video, VideoDetail, Canvas } from "components";
 import { Navbar, Footer } from "components";
 
 import { Url } from "constant";
@@ -18,6 +17,15 @@ import { useStore } from "hooks";
 import { theme } from "style";
 import { utils } from "util/utils";
 // import { goodbye } from "static/music";
+
+const Main = React.lazy(() => import("../components/page/main/Main"));
+const Member = React.lazy(() => import("../components/page/member/Member"));
+const Album = React.lazy(() => import("../components/page/album/Album"));
+const Video = React.lazy(() => import("../components/page/video/Video"));
+const VideoDetail = React.lazy(() =>
+  import("../components/page/video/VideoDetail")
+);
+const Canvas = React.lazy(() => import("../components/page/canvas/Canvas"));
 
 const MainContainer = styled.div`
   width: 100%;
@@ -54,26 +62,42 @@ const App = () => {
             <Loading />
             <Toast />
 
-            {!utils.isMobile() && <AnimateTitle active={_.isEqual(path, "/")} />}
+            {!utils.isMobile() && (
+              <AnimateTitle active={_.isEqual(path, "/")} />
+            )}
 
-            <Routes>
-              <Route path={`${Url.ROOT}`} element={<Main />} />
-              <Route path={`${Url.MEMBER}`} element={<Member />} />
-              <Route path={`${Url.ALBUM}`} element={<Album />} />
-              <Route path={`${Url.MUSIC}`} element={<Video />} />
-              <Route path={`${Url.MUSIC}/:videoId`} element={<VideoDetail />} />
-              <Route path={`${Url.QUEENDOM}`} element={<Video />} />
-              <Route path={`${Url.QUEENDOM}/:videoId`} element={<VideoDetail />} />
-              <Route path={`${Url.BGCLIP}`} element={<Video />} />
-              <Route path={`${Url.BGCLIP}/:videoId`} element={<VideoDetail />} />
-              <Route path={`${Url.CANVAS}`} element={<Canvas />} />
-              <Route path={`${Url.BOARD}`} element={<CanvasBoard />} />
-              <Route path={`${Url.BOARD}/:boardId`} element={<CanvasDetail />} />
-              <Route path={`${Url.LOGIN}`} element={<Login />} />
-              <Route path={`${Url.SUCCESS}`} element={<Success />} />
-              <Route path={`${Url.REGISTER}`} element={<Register />} />
-              <Route path={`*`} element={<Main />} />
-            </Routes>
+            <Suspense fallback={null}>
+              <Routes>
+                <Route path={`${Url.ROOT}`} element={<Main />} />
+                <Route path={`${Url.MEMBER}`} element={<Member />} />
+                <Route path={`${Url.ALBUM}`} element={<Album />} />
+                <Route path={`${Url.MUSIC}`} element={<Video />} />
+                <Route
+                  path={`${Url.MUSIC}/:videoId`}
+                  element={<VideoDetail />}
+                />
+                <Route path={`${Url.QUEENDOM}`} element={<Video />} />
+                <Route
+                  path={`${Url.QUEENDOM}/:videoId`}
+                  element={<VideoDetail />}
+                />
+                <Route path={`${Url.BGCLIP}`} element={<Video />} />
+                <Route
+                  path={`${Url.BGCLIP}/:videoId`}
+                  element={<VideoDetail />}
+                />
+                <Route path={`${Url.CANVAS}`} element={<Canvas />} />
+                <Route path={`${Url.BOARD}`} element={<CanvasBoard />} />
+                <Route
+                  path={`${Url.BOARD}/:boardId`}
+                  element={<CanvasDetail />}
+                />
+                <Route path={`${Url.LOGIN}`} element={<Login />} />
+                <Route path={`${Url.SUCCESS}`} element={<Success />} />
+                <Route path={`${Url.REGISTER}`} element={<Register />} />
+                <Route path={`*`} element={<Main />} />
+              </Routes>
+            </Suspense>
             <Footer />
           </MainContainer>
         </ThemeProvider>
